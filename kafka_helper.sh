b696d9a1d1f2e8232db222686e32f510783bd1dd
Brandon Payne <bpayne@trihydro.com>
2023-08-29 13:51:35 -0600

    Initial instructions and resources

---
+++ kafka_helper.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+# Helpers
+GREEN='\033[92m'
+RED='\033[91m'
+NC='\033[0m'
+CYAN='\033[96m'
+YELLOW='\033[93m'
+TICK="[${GREEN}✓${NC}]"
+CROSS="[${RED}✗${NC}]"
+INFO="[${CYAN}i${NC}]"
+ALERT="[${YELLOW}⚠${NC}]"
+
+listtopics() {
+    docker run --tty --network codingchallenge_default confluentinc/cp-kafkacat kafkacat -b kafka:9092 -L
+}
+
+readdata() {
+    docker run --tty \
+    --network codingchallenge_default \
+    confluentinc/cp-kafkacat \
+    kafkacat -b kafka:9092 -C \
+    -t WeatherData
+}
+
+pushdata() {
+    docker run --network codingchallenge_default \
+    --volume ./weatherdata.json:/data/weatherdata.json \
+    confluentinc/cp-kafkacat \
+    kafkacat -b kafka:9092 \
+    -t WeatherData \
+    -P -l /data/weatherdata.json
+}
+
+help() {
+    echo -e "Usage: kafka_helper [options]
+
+Example: kafka_helper topics
+
+Options:
+    ${CYAN}topics${NC}      List all Kafka topics
+    ${CYAN}pushdata${NC}    Push data from weatherdata.json onto Kafka topic
+    ${CYAN}readweather${NC} Read data from Kafka topic
+    "
+    exit 0
+}
+
+case "${1}" in
+    "topics") listtopics ;;
+    "pushdata") pushdata ;;
+    "readweather") readdata ;;
+    *) help ;;
+esac
